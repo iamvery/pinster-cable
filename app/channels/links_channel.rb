e61525138cd3fa2158851640b78d2b74614b8719
Jay Hayes <ur@iamvery.com>
2016-02-21 11:59:47 -0600

    Stream links

--- app/channels/links_channel.rb
+++ app/channels/links_channel.rb
@@ -1,10 +1,12 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class LinksChannel < ApplicationCable::Channel
+  STREAM = :links
+
   def subscribed
-    # stream_from "some_channel"
+    stream_from(STREAM)
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
   end
 end
