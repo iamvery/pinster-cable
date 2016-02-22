41b23a7169e33a60d456a5611c09f8bab7e8edbf
Jay Hayes <ur@iamvery.com>
2016-02-22 11:10:46 -0600

    Change stream name to string
    Using a symbol causes the redis backend to fail silently. Looking for a
    way to make this better in Rails.
    
    https://github.com/rails/rails/pull/23811

--- app/channels/links_channel.rb
+++ app/channels/links_channel.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class LinksChannel < ApplicationCable::Channel
-  STREAM = :links
+  STREAM = "links"
 
   def subscribed
     stream_from(STREAM)
