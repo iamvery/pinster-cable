a2ff4d14b29262b2d2b60fef18f8c97b5e08aa05
Jay Hayes <ur@iamvery.com>
2016-03-02 21:36:22 -0500

    Configure test suite to use fake open graph lib

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,10 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require "capybara/rspec"
+require "support/fake_open_graph"
 # Add additional requires below this line. Rails is not loaded until this point!
+
+Rails.configuration.open_graph_lib = FakeOpenGraph
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
