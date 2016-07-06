f998b3a7b364a7d49467c434cb06ab3b5344de13
Jay Hayes <ur@iamvery.com>
2016-07-06 12:04:37 -0500

    Configure test suite to always use fake open graph adapter

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,9 @@ require 'rspec/rails'
 require "capybara/rspec"
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require "support/fake_open_graph"
+Rails.configuration.open_graph = FakeOpenGraph
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
