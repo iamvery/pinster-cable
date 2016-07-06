312f37955f11dedf5d86db87f3e3aa3a229fac61
Jay Hayes <ur@iamvery.com>
2016-07-06 12:01:29 -0500

    Inject fake open graph in link model test
    This decouples the model test from the Internet. However, the acceptance
    test still uses the real open graph library, because nothing is being
    injected in the view's call to Link#title.

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,10 +1,11 @@
 require "rails_helper"
+require "support/fake_open_graph"
 
 RSpec.describe Link do
   describe "#title" do
     it "returns the open graph title" do
       link = Link.new(url: "http://google.com")
-      title = link.title
+      title = link.title(FakeOpenGraph)
 
       expect(title).to eq("Google")
     end
