112fbc2d93e6e39c8b75231a704ac43e84ba8b7f
Jay Hayes <ur@iamvery.com>
2016-07-06 12:05:18 -0500

    We no longer need to use the injection interface for model test
    Since the test suite is configured to use the fake, we don't have to
    explicitly inject it in the model test anymore. This feels OK, because
    all code is self-documenting. However, leaving the injectable interface
    is convenient as it keeps the implementation of Link decoupled from a
    particular open graph implementation.

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,11 +1,10 @@
 require "rails_helper"
-require "support/fake_open_graph"
 
 RSpec.describe Link do
   describe "#title" do
     it "returns the open graph title" do
       link = Link.new(url: "http://google.com")
-      title = link.title(FakeOpenGraph)
+      title = link.title
 
       expect(title).to eq("Google")
     end
